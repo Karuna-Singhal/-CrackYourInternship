634dac329baccb311238eb6eb8ff0c4ac96fc73a
Karuna <singhalkaruna73@gmail.com>
2024-07-20 10:47:23 +0530

    save

---
+++ Tree/5_symmetric_tree.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool solve(TreeNode *root1, TreeNode *root2)
+{
+  if (root1 == NULL && root2 == NULL)
+    return true;
+  else if (root1 == NULL || root2 == NULL)
+    return false;
+  else if (root1->val != root2->val)
+    return false;
+
+  bool left = solve(root1->left, root2->right);
+  bool right = solve(root1->right, root2->left);
+
+  return left && right;
+}
+
+bool isSymmetric(TreeNode *root)
+{
+  if (root == NULL)
+    return true;
+
+  return solve(root->left, root->right);
+}
+
+int main()
+{
+  TreeNode *root = new TreeNode(1);
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(2);
+  root->left->left = new TreeNode(3);
+  root->left->right = new TreeNode(4);
+  root->right->left = new TreeNode(4);
+  root->right->right = new TreeNode(3);
+  bool ans = isSymmetric(root);
+
+  cout << ans;
+
+  return 0;
+}
